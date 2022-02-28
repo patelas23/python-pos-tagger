8b1e549beb54d9486fc0db3c2b6e5e81489380b9
Ankur Patel <patelas23@vcu.edu>
2022-02-28 17:00:17 -0500

    initial commit

---
+++ tagger.py
@@ -0,0 +1,20 @@
+# Author: Ankur Patel
+# 
+# Python program to tag parts of speech after 
+#   constructing a "most likely tag" model based 
+#   on supplied training data. 
+# Inputs a POS-tagged file, recording the most likely 
+#  tags for each word. Output -> use model to POS tag a test file
+# 
+
+def train_model():
+    pass
+
+def apply_tags():
+    pass
+
+if __name__ == '__main__':
+    import sys
+    training_file = ""
+    test_file = ""
+    file_with_tags = ""
