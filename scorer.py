4253305d3d1bdd17f76faf13773f036a3db3d6e4
Ankur Patel <patelas23@vcu.edu>
2022-03-15 21:49:43 -0400

    present accuracy score and confusion matrix for tagged text

--- scorer.py
+++ scorer.py
@@ -10,21 +10,30 @@
 #       rows (actual): sequence of tags from gold standard
 #       cols(predicted): sequence of tagger.py tags
 #
-import pandas as pd 
+import pandas as pd
+import re 
 from sklearn import metrics
 import sys
 
-def parse_text(corpus):
-    pass
+# Helper function to extract list of tags from tagged text
+# IN: 
+#   file_contents: raw string
+# OUT: 
+#   tag_list: list containing tags
+def extract_tags(file_contents):
+    tagger_pair_pattern = r"[\S]+/([\S]+)"
+    file_contents = re.sub(r"[\[\]]", " ", file_contents)
+    return re.findall(tagger_pair_pattern, file_contents)
+    
 
-def score_pos_tags(predicted: list, actual: list):
+def score_pos_tags(actual: list, predicted: list):
     # Cast each list as a pandas series for analysis
+    actual_series = pd.Series(actual, name='Actual')
     predicted_series = pd.Series(predicted, name='Predicted')
-    actual_series = pd.Series(actual, name="Actual")
     # zip together both lists into a 2d array
-    confusion_mat = metrics.confusion_matrix(actual_series, predicted_series)
-    sys.stdout.write(pd.crosstab(actual_series, predicted_series))
-    sys.stdout.write("Accuracy score: " + metrics.accuracy_score(actual_series, predicted_series))
+    sys.stdout.write("Accuracy score: " + str(metrics.accuracy_score(actual_series, predicted_series)) + "\n")
+    sys.stdout.write(str(pd.crosstab(actual_series, predicted_series)))
+    
 
 if __name__ == '__main__':
     
@@ -34,6 +43,9 @@ if __name__ == '__main__':
     tagged_corpus = ""
     test_key_corpus = ""
     
+    tagged_data = []
+    test_data = []
+    
     with open(tagged_file_name) as file:
         tagged_corpus = file.read()
         
@@ -41,3 +53,7 @@ if __name__ == '__main__':
     with open(test_key_file_name) as file:
         test_key_corpus = file.read()
     
+    tagged_data = extract_tags(tagged_corpus)
+    test_data = extract_tags(test_key_corpus)
+    
+    score_pos_tags(actual=test_data, predicted=tagged_data)
